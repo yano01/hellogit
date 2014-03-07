47547ab84bba39d6e98b28164c5af187be74354a
yano_y01 <yano_y01@A010199.pci-sol.local>
2014-03-07 09:27:28 +0900

    最初のコミット

---
+++ Sample2/src/com/pci/Sample2.java
@@ -0,0 +1,10 @@
+package com.pci;
+
+import java.io.FileInputStream;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		FileInputStream fis = new FileInputStream(args[0]);//Exceptionを継承した例外が発生する処理
+	}
+}
